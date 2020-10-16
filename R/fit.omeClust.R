c88a27bd773c392df03da237d88f0ea2bf2e1563
SuvoChatterjee <suvo2chatterjee@gmail.com>
2020-10-16 18:27:28 -0400

    omeClust changed name

--- R/fit.omeClust.R
+++ R/fit.omeClust.R
@@ -1,4 +1,4 @@
-fit.m2clust<-function(resolution,workingDirectory)
+fit.omeClust<-function(resolution,workingDirectory)
 {
   
   #########################
@@ -31,7 +31,7 @@ fit.m2clust<-function(resolution,workingDirectory)
   
   data.path<-paste(workingDirectory,"Input","Distance_Data",sep="\\")
   truth.path<-paste(workingDirectory,"Input","Truth_meta",sep="\\")
-  m2clust.path<-paste (workingDirectory,"m2clust",folder,sep="\\")
+  omeClust.path<-paste (workingDirectory,"omeClust",folder,sep="\\")
   
   ##############################
   ### Getting DataNames List ###
@@ -39,7 +39,7 @@ fit.m2clust<-function(resolution,workingDirectory)
   
   data.list<-list.files(data.path)
   truth.list<-list.files(truth.path)
-  m2clust.list<-list.files(m2clust.path)
+  omeClust.list<-list.files(omeClust.path)
   
   final.output<-NULL
   for(i in 1:length(data.list))
@@ -52,17 +52,17 @@ fit.m2clust<-function(resolution,workingDirectory)
     input.data<-data.frame(read.table(file.path(data.path,data.list[i])))
     input.data<-as.dist(input.data,diag=T)
     input.truth<-data.frame(read.table(file.path(truth.path,truth.list[i])))
-    input.m2clust<-data.frame(read.table(file.path(m2clust.path,m2clust.list[i],
+    input.omeClust<-data.frame(read.table(file.path(omeClust.path,omeClust.list[i],
                                                    "clusters.txt"),h=T))
     
     ################################
-    ### Getting m2clust clusters ###
+    ### Getting omeClust clusters ###
     ################################
     
     mem.mat<-NULL
-    for (j in 1:nrow(input.m2clust))
+    for (j in 1:nrow(input.omeClust))
     {
-      temp<-input.m2clust[j,]
+      temp<-input.omeClust[j,]
       cl<-str_sub(temp$cluster,2,str_length(temp$cluster))
       mem<-unlist(strsplit(as.character(temp$members), ";"))
       temp1<-data.frame(cbind(members=mem,cluster=rep(cl,length(mem))))
@@ -74,7 +74,7 @@ fit.m2clust<-function(resolution,workingDirectory)
     ### Creating OutputLabels ###
     #############################
     
-    new.filename<-substr(m2clust.list[i],1,nchar(m2clust.list[i])-4)
+    new.filename<-substr(omeClust.list[i],1,nchar(omeClust.list[i])-4)
     stringLabel<-c('clusters','features','sample','alpha','sd')
     named.files<-str_split(new.filename,pattern="_")[[1]]
     names(named.files)<-stringLabel
@@ -90,7 +90,7 @@ fit.m2clust<-function(resolution,workingDirectory)
     
     sil.sc<-silhouette(unname(sapply(mem.mat$cluster,as.numeric)),input.data)
     sil.sc<-round(mean(sil.sc[,"sil_width"]),2)
-    major.clusters<-nrow(input.m2clust[input.m2clust$resolution_score>=0.10,])
+    major.clusters<-nrow(input.omeClust[input.omeClust$resolution_score>=0.10,])
     measure.df<-cluster_eval(truth=input.truth$Ground.Truth,
                              predicted=mem.mat$cluster,
                              nclusters,major.clusters)
@@ -103,7 +103,7 @@ fit.m2clust<-function(resolution,workingDirectory)
     mis_rate<-abs(clusters-size)/sample
     output.df<-data.frame(clusters=clusters,features=features,
                           sample=sample,Dist=alpha,SD=sd,
-                          method="m2clust",
+                          method="omeClust",
                           major.clusters=major.clusters,
                           Silhoutte.score=sil.sc,residual=mis_rate)
     output.df<-data.frame(cbind(output.df,measure.df),check.names=F)
